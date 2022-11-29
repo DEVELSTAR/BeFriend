c9dbb2d650086493dba72b8ebfb900ba1ab1f89b
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-11-29 17:58:46 +0530

    added more fields to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
 
     def index
-    	@users = User.all
+      @users = User.all
     end
 
 	def show
@@ -28,5 +27,12 @@ class UsersController < ApplicationController
 	  @user = User.find(params[:id])
 	  current_user.followed_users.find_by(followee_id: @user.id).destroy
 	  redirect_back(fallback_location: user_path(@user))
+	end
+
+
+	private
+
+	def user_params
+	  params.require(:user).permit(:name, :bio, :email, :phone)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users
   resources :posts
   resources :follows
   

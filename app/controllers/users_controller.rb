689ac1ff73cf7b06f325ba2500bf0b2c8649a675
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-11-29 09:12:09 +0530

    added devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
 
 	def show
+	  @user  = User.find(params[:id])
 	end
 
-	def edit
+    def edit
+	  @user = User.find(params[:id])
 	end
 
 	def update
+	  current_user.update(user_params)
+	  redirect_to current_user
 	end
-
+	
 end

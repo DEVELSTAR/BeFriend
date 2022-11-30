81f6f742ed9cf909c504d86e50ccf84f24c2b72d
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-11-30 18:16:03 +0530

    minor changes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+	def create
+	    @comment = Comment.create(comment_params)
+	    @comment.user = current_user
+	    redirect_to request.referrer
+	end
+
+	def destroy
+	    @comment = Comment.find(params[:id])
+	    @comment.destroy
+	    redirect_to request.referrer
+    end
+
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:body, :post_id, :user_id)
+	end
+end

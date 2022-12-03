68226a221a215d35a4ad6cf8e7b076d42f592da3
Mohd Akibuddin <akeebudeen786@gmail.com>
2022-12-03 15:26:54 +0530

    post descending order by creation time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,15 +9,15 @@ class HomeController < ApplicationController
   end
 
   def private_post
-    @posts = current_user.posts
+    @posts = current_user.posts.all.order(created_at: :desc)
   end
 
   def liked_post
-    @posts = current_user.liked_posts
+    @posts = current_user.liked_posts.all.order(created_at: :desc)
   end
 
   def my_posts
-    @posts = current_user.posts
+    @posts = current_user.posts.all.order(created_at: :desc)
   end
 
 end
